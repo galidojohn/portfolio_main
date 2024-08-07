4f0b276c827b678171153551ef35dd5be9fc5e37
John Glenn <nnam11821@gmail.com>
2024-08-07 18:19:55 +0800

    fifth commit

--- src/components/navbar/sidebar/Sidebar.jsx
+++ src/components/navbar/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import Links from "./links/Links";
 import "./sidebar.scss";
-import ToggleButton from "./toggleButton/toggleButton";
+import ToggleButton from "./ToggleButton/ToggleButton";
 
 const variants = {
     open: {
